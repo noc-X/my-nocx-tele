870421c8b8d22cb7ada74a818a5b19526f43ec7a
noc-X <amoydharma@gmail.com>
2024-07-22 01:34:02 +0700

    full

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import anime from 'animejs';
-import { Howl } from 'howler';
 import Menu from './components/Menu';
 import Stake from './components/Stake';
 import Presale from './components/Presale';
@@ -9,42 +7,40 @@ import Partnership from './components/Partnership';
 import Airdrop from './components/Airdrop';
 import Task from './components/Task';
 import Invite from './components/Invite';
-import WalletConnect from './components/WalletConnect';
-import AdminTasks from './components/AdminTasks';
-import './index.css';
+import { Howl } from 'howler';
+import anime from 'animejs';
+import './App.css';
 
 const App: React.FC = () => {
   useEffect(() => {
-    // Anime.js effect
-    anime({
-      targets: 'h1',
-      translateX: 250,
-      duration: 800,
-    });
-
-    // Howler.js background music
     const sound = new Howl({
       src: ['path/to/your/music.mp3'],
+      autoplay: true,
+      loop: true,
     });
+
     sound.play();
+
+    anime({
+      targets: '.menu-item img',
+      translateY: -10,
+      direction: 'alternate',
+      loop: true,
+      easing: 'easeInOutSine',
+    });
   }, []);
 
   return (
     <Router>
-      <div>
-        <h1>My Miniapp</h1>
-        <Routes>
-          <Route path="/" element={<Menu />} />
-          <Route path="/stake" element={<Stake />} />
-          <Route path="/presale" element={<Presale />} />
-          <Route path="/partnership" element={<Partnership />} />
-          <Route path="/airdrop" element={<Airdrop />} />
-          <Route path="/task" element={<Task />} />
-          <Route path="/invite" element={<Invite />} />
-          <Route path="/wallet-connect" element={<WalletConnect />} />
-          <Route path="/admin/tasks" element={<AdminTasks />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/stake" element={<Stake />} />
+        <Route path="/presale" element={<Presale />} />
+        <Route path="/partnership" element={<Partnership />} />
+        <Route path="/airdrop" element={<Airdrop />} />
+        <Route path="/task" element={<Task />} />
+        <Route path="/invite" element={<Invite />} />
+      </Routes>
     </Router>
   );
 };
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Menu.css'; // Buat file ini untuk styling khusus Menu
+import './Menu.css';
 
-const Menu = () => {
+const Menu: React.FC = () => {
   return (
     <div className="menu-container">
       <Link to="/stake">
         <div className="menu-item">
-          <img src="./assets/stake.png" alt="Stake" />
-          <div>STAKE</div>
+          <img src="/assets/stake.png" alt="Stake" />
+          <div className="menu-label">STAKE</div>
         </div>
       </Link>
       <Link to="/presale">
         <div className="menu-item">
-          <img src="./assets/presale.png" alt="Presale" />
-          <div>PRESALE</div>
+          <img src="/assets/presale.png" alt="Presale" />
+          <div className="menu-label">PRESALE</div>
         </div>
       </Link>
       <Link to="/partnership">
         <div className="menu-item">
-          <img src="./assets/patner.png" alt="Partnership" />
-          <div>PARTNERSHIP</div>
+          <img src="/assets/partnership.png" alt="Partnership" />
+          <div className="menu-label">PARTNERSHIP</div>
         </div>
       </Link>
       <Link to="/airdrop">
         <div className="menu-item">
-          <img src="./assets/airdrop.png" alt="Airdrop" />
-          <div>AIRDROP</div>
+          <img src="/assets/airdrop.png" alt="Airdrop" />
+          <div className="menu-label">AIRDROP</div>
         </div>
       </Link>
       <Link to="/task">
         <div className="menu-item">
-          <img src="task.png" alt="Task" />
-          <div>TASK</div>
+          <img src="/assets/task.png" alt="Task" />
+          <div className="menu-label">TASK</div>
         </div>
       </Link>
       <Link to="/invite">
         <div className="menu-item">
-          <img src="invite.png" alt="Invite" />
-          <div>INVITE</div>
+          <img src="/assets/invite.png" alt="Invite" />
+          <div className="menu-label">INVITE</div>
         </div>
       </Link>
     </div>
