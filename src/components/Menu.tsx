bba7a7ebf84b2b27a0bf0f83d860e76699ad79b2
noc-X <amoydharma@gmail.com>
2024-07-22 01:57:15 +0700

    all

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,42 +5,36 @@ import './Menu.css';
 const Menu: React.FC = () => {
   return (
     <div className="menu-container">
-      <Link to="/stake">
-        <div className="menu-item">
+      <div className="row">
+        <Link to="/stake" className="menu-item">
           <img src="/assets/stake.png" alt="Stake" />
           <div className="menu-label">STAKE</div>
-        </div>
-      </Link>
-      <Link to="/presale">
-        <div className="menu-item">
+        </Link>
+        <Link to="/presale" className="menu-item">
           <img src="/assets/presale.png" alt="Presale" />
           <div className="menu-label">PRESALE</div>
-        </div>
-      </Link>
-      <Link to="/partnership">
-        <div className="menu-item">
+        </Link>
+      </div>
+      <div className="row">
+        <Link to="/partnership" className="menu-item">
           <img src="/assets/partnership.png" alt="Partnership" />
           <div className="menu-label">PARTNERSHIP</div>
-        </div>
-      </Link>
-      <Link to="/airdrop">
-        <div className="menu-item">
+        </Link>
+        <Link to="/airdrop" className="menu-item">
           <img src="/assets/airdrop.png" alt="Airdrop" />
           <div className="menu-label">AIRDROP</div>
-        </div>
-      </Link>
-      <Link to="/task">
-        <div className="menu-item">
+        </Link>
+      </div>
+      <div className="row">
+        <Link to="/task" className="menu-item">
           <img src="/assets/task.png" alt="Task" />
           <div className="menu-label">TASK</div>
-        </div>
-      </Link>
-      <Link to="/invite">
-        <div className="menu-item">
+        </Link>
+        <Link to="/invite" className="menu-item">
           <img src="/assets/invite.png" alt="Invite" />
           <div className="menu-label">INVITE</div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
   );
 };
