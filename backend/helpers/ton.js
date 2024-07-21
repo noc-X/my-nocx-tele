de1ab5b7833bf93f125991bc994b76c1a02394e8
noc-X <amoydharma@gmail.com>
2024-07-21 23:11:11 +0700

    perbaikan client ton

--- backend/helpers/ton.js
+++ backend/helpers/ton.js
@@ -1,9 +1,21 @@
-const { TonClient } = require('ton-client-node-js');
+const { TonClient } = require("@tonclient/core");
+const { libNode } = require("@tonclient/lib-node");
 
-const client = new TonClient({
-  network: {
-    server_address: 'main.ton.dev'
+TonClient.useBinaryLibrary(libNode);
+
+async function initializeClient() {
+  try {
+    const client = new TonClient({
+      network: {
+        endpoints: ['https://main.ton.dev']
+      }
+    });
+
+    console.log("TON Client initialized");
+    // Perform additional operations with the client if needed
+  } catch (err) {
+    console.error("Failed to initialize TON Client", err);
   }
-});
+}
 
-module.exports = client;
+initializeClient();
